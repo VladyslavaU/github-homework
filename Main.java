4c286d35533ec837be243776e34b0abc1df5d424
VladyslavaU <vuvarova@outlook.com>
2021-03-04 21:29:27 +0200

    Added the print() method that accepts only one paramenter

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-public class Main{
-    public static void main (String[] args){
+public class Main {
+    public static void main(String[] args) {
         System.out.println("Hello, world!");
+    }
+
+    public static void print(String input) {
+        System.out.println(input);
     }
 }
